bfdc8eb206bd2f999730962fd65599e27ceb285b
Josh Juran <josh@ripple.com>
2015-01-26 10:53:23 -0800

    Handle `algorithm` parameter and ed25519 keys

--- src/ripple/rpc/impl/KeypairForSignature.cpp
+++ src/ripple/rpc/impl/KeypairForSignature.cpp
@@ -18,6 +18,7 @@
 //==============================================================================
 
 #include <BeastConfig.h>
+#include <ripple/crypto/SignatureAlgorithm.h>
 #include <ripple/rpc/impl/KeypairForSignature.h>
 
 namespace ripple {
@@ -25,20 +26,75 @@ namespace RPC {
 
 SignatureKeypair KeypairForSignature (Json::Value const& params)
 {
-    if (! params.isMember ("secret"))
+    bool const has_algorithm  = params.isMember ("algorithm");
+    bool const has_passphrase = params.isMember ("passphrase");
+    bool const has_secret     = params.isMember ("secret");
+    bool const has_seed       = params.isMember ("seed");
+    bool const has_seed_hex   = params.isMember ("seed_hex");
+
+    int const n_secrets = has_passphrase + has_secret + has_seed + has_seed_hex;
+
+    if (n_secrets == 0)
         throw RPC::missing_field_error ("secret");
+
+    if (n_secrets > 1)
+    {
+        // `passphrase`, `secret`, `seed`, and `seed_hex` are mutually exclusive.
+        throw rpcError (rpcBAD_SECRET);
+    }
+
+    if (has_algorithm  &&  has_secret)
+    {
+        // `secret` is deprecated.
+        throw rpcError (rpcBAD_SECRET);
+    }
+
+    SignatureAlgorithm a = secp256k1;
 
     RippleAddress seed;
 
+
+    if (has_algorithm)
+    {
+        // `algorithm` must be valid if present.
+
+        a = AlgorithmFromString (params["algorithm"].asString());
+
+        if (isInvalid (a))
+        {
+            throw rpcError (rpcBAD_SEED);
+        }
+
+        seed = GetSeedFromRPC (params);
+    }
+    else
     if (! seed.setSeedGeneric (params["secret"].asString ()))
         throw RPC::make_error (rpcBAD_SEED,
             RPC::invalid_field_message ("secret"));
 
     SignatureKeypair result;
 
-    RippleAddress generator = RippleAddress::createGeneratorPublic (seed);
-    result.secretKey.setAccountPrivate (generator, seed, 0);
-    result.publicKey.setAccountPublic (generator, 0);
+    if (a == secp256k1)
+    {
+        RippleAddress generator = RippleAddress::createGeneratorPublic (seed);
+        result.secretKey.setAccountPrivate (generator, seed, 0);
+        result.publicKey.setAccountPublic (generator, 0);
+    }
+    else
+    {
+        uint256 secretkey = KeyFromSeed (seed.getSeed());
+
+        Blob ed25519_key (33);
+        ed25519_key[0] = 0xED;
+
+        memcpy (&ed25519_key[1], secretkey.data(), secretkey.size());
+        result.secretKey.setAccountPrivate (ed25519_key);
+
+        ed25519_publickey (secretkey.data(), &ed25519_key[1]);
+        result.publicKey.setAccountPublic (ed25519_key);
+
+        secretkey.zero();  // security erase
+    }
 
     return result;
 }
